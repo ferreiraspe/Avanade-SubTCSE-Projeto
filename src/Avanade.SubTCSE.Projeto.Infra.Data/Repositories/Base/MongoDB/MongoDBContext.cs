0289224f88614c4fef6c53486e7203e0b0cb8e66
Edson <ferreiraspe@gmail.com>
2021-07-07 08:06:10 -0300

    criando mongodbcontext

--- src/Avanade.SubTCSE.Projeto.Infra.Data/Repositories/Base/MongoDB/MongoDBContext.cs
+++ src/Avanade.SubTCSE.Projeto.Infra.Data/Repositories/Base/MongoDB/MongoDBContext.cs
@@ -5,6 +5,26 @@ namespace Avanade.SubTCSE.Projeto.Infra.Data.Repositories.Base.MongoDB
 {
     public class MongoDBContext : IMogoDBContext
     {
+        private readonly IMongoDatabase _mongoDatabase;
+
+        public MongoDBContext()
+        {
+            MongoClientSettings mongoClientSettings = MongoClientSettings
+                .FromUrl(new MongoUrl(""));
+
+            mongoClientSettings.SslSettings =
+                new SslSettings()
+                {
+                    EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12
+                };
+
+            MongoClient mongoClient = new MongoClient(settings: mongoClientSettings);
+
+            IMongoDatabase mongoDatabase = mongoClient.GetDatabase("");
+
+            mongoDatabase.GetCollection
+        }
+
         public IMongoCollection<TEntity> GetColletion<TEntity>(string collection)
         {
             throw new System.NotImplementedException();
